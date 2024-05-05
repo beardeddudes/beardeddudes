34c2f697cb41f7f8e7328f1f72c9eebd664fcc27
Dimmasay <kruhliakds@gmail.com>
2024-05-05 16:56:36 +0300

    build (w3s): add the layout element `Header`

--- packages/w3s/src/layouts/Header/Header.tsx
+++ packages/w3s/src/layouts/Header/Header.tsx
@@ -1,7 +1,25 @@
 import React, { FC } from "react"
 
+import "./Header.scss"
 const Header: FC = () => {
-  return <React.Fragment></React.Fragment>
+  return (
+    <div className={"header__container"}>
+      <a href={"/"} className={"header__logo"}>
+        cactus
+      </a>
+      <div className={"header__navigation"}>
+        <button className={"header__navigation-btn"}>Services</button>
+        <button className={"header__navigation-btn"}>Technologies</button>
+        <button className={"header__navigation-btn"}>Process</button>
+        <button className={"header__navigation-btn"}>Works</button>
+        <button className={"header__navigation-btn"}>About</button>
+        <button className={"header__navigation-btn"}>Contacts</button>
+      </div>
+      <button className={"header__project-btn"}>
+        <span>Start a project</span>
+      </button>
+    </div>
+  )
 }
 
 export default Header
