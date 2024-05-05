7c6d6a640c90cba38c070f2f6767f31b85eb3b24
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-05-05 22:34:22 +0300

    refactor (w3s): sort imports

--- packages/w3s/src/pages/MainPage.tsx
+++ packages/w3s/src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react"
+
 import DevelopmentProcess from "./../containers/DevelopmentProcess/DevelopmentProcess"
 import Hero from "./../containers/Hero/Hero"
 import Technologies from "./../containers/Technologies/Technologies"
