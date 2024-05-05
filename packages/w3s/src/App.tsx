1600cfe0d1c3c7d207f77b80b693b8535466425a
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-05-05 22:39:18 +0300

    feat (w3s): add `internationalization`

--- packages/w3s/src/App.tsx
+++ packages/w3s/src/App.tsx
@@ -1,14 +1,35 @@
+import i18next from "i18next"
 import { FC } from "react"
+import { I18nextProvider } from "react-i18next"
 import { RouterProvider } from "react-router-dom"
 
 import { AppProvider } from "./contexts/App"
 import Router from "./pages/Router"
 
+import globalEn from "./locales/en/global.json"
+import globalUa from "./locales/ua/global.json"
+
+i18next.init({
+  fallbackLng: "en",
+  interpolation: { escapeValue: false },
+  lng: "auto",
+  resources: {
+    en: {
+      global: globalEn,
+    },
+    ua: {
+      global: globalUa,
+    },
+  },
+})
+
 const App: FC = () => {
   return (
-    <AppProvider>
-      <RouterProvider router={Router} />
-    </AppProvider>
+    <I18nextProvider i18n={i18next}>
+      <AppProvider>
+        <RouterProvider router={Router} />
+      </AppProvider>
+    </I18nextProvider>
   )
 }
 
