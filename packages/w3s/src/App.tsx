1c34df37e5c96b00d4a0bb8c8239838ec2381128
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-05-09 17:16:02 +0300

    refactor (w3s): remove translations

--- packages/w3s/src/App.tsx
+++ packages/w3s/src/App.tsx
@@ -1,31 +1,14 @@
-import i18next from "i18next"
 import { FC } from "react"
-import { I18nextProvider } from "react-i18next"
 import { RouterProvider } from "react-router-dom"
 
 import { AppProvider } from "./contexts/App"
 import Router from "./pages/Router"
 
-import globalEn from "./locales/en/global.json"
-
-i18next.init({
-  fallbackLng: "en",
-  lng: "auto",
-  interpolation: { escapeValue: false },
-  resources: {
-    en: {
-      global: globalEn,
-    },
-  },
-})
-
 const App: FC = () => {
   return (
-    <I18nextProvider i18n={i18next}>
-      <AppProvider>
-        <RouterProvider router={Router} />
-      </AppProvider>
-    </I18nextProvider>
+    <AppProvider>
+      <RouterProvider router={Router} />
+    </AppProvider>
   )
 }
 
