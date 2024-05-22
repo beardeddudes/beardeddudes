793b8c9214e93aed95203ec909c5f2e0e1b3264a
Dimmasay <kruhliakds@gmail.com>
2024-05-23 02:31:32 +0300

    refactor (w3s) : update hook `useScrollLock`

--- packages/w3s/src/hooks/useScrollLock.tsx
+++ packages/w3s/src/hooks/useScrollLock.tsx
@@ -1,21 +1,31 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-const useScrollLock = (flag: boolean): void => {
+interface IUseScrollLock {
+  isLocked: boolean
+}
+const useScrollLock = (flag: boolean): IUseScrollLock => {
+  const [isLocked, setIsLocked] = useState<boolean>(false)
   useEffect(() => {
     const root = document.getElementById("root")
 
     if (root) {
       document.body.style.overflow = flag ? "hidden" : "auto"
       // root.style.scrollbarGutter = flag ? "stable" : "auto"
+      setIsLocked(true)
     }
 
     return () => {
       if (root) {
+        setIsLocked(false)
         document.body.style.overflow = "auto"
         //   root.style.scrollbarGutter = "auto"
       }
     }
   }, [flag])
+
+  return {
+    isLocked,
+  }
 }
 
 export { useScrollLock }
